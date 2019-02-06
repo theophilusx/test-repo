6d36ca70a1d1891e82263f91286144d6612b86c9
Tim Cross <tcross8@une.edu.au>
2019-02-06 19:00:53 +1100

    Submitting plan Bad Plan for approval

---
+++ changes/Bad-Plan.sql
@@ -0,0 +1,13 @@
+
+-- Name:   Bad Plan
+-- Author: Tim Cross <tcross8@une.edu.au>
+-- Date:   2019-02-06 18:59:52
+-- Source: changes/Bad-Plan.sql
+-- Type:   Changes
+
+BEGIN;
+
+-- changes go here
+
+COMMIT;
+
---
+++ verify/Bad-Plan.sql
@@ -0,0 +1,9 @@
+
+-- Name:   Bad Plan
+-- Author: Tim Cross <tcross8@une.edu.au>
+-- Date:   2019-02-06 18:59:52
+-- Source: verify/Bad-Plan.sql
+-- Type:   Verify
+
+-- verify code go here
+
