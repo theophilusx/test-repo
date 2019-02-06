f09425fe0880e6c3a7829b244033bde6eda70a93
Tim Cross <tcross8@une.edu.au>
2019-02-07 10:37:57 +1100

    Rejection of plan Bad Plan

--- changes/Bad-Plan.sql
+++ changes/Bad-Plan.sql
@@ -11,3 +11,14 @@ BEGIN;
 
 COMMIT;
 
+
+-- Tim Cross <tcross8@une.edu.au>
+-- 2019-02-07 10:37:33
+--
+-- This change has been rejected because
+-- 
+-- - It breaks existing API without sufficient justification
+-- - Lacks evidence of sufficient testing
+-- - Fails to use existing functions (re-invents the wheel)
+-- - Is large enough it requires a valid verification and rollback script
+-- 
