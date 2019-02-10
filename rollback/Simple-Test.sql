813ebb56cd378d86de904c826f098d5883b48d4f
Tim Cross <tcross@une.edu.au>
2019-02-10 14:44:16 +1100

    Submitting plan Simple Test for approval

---
+++ rollback/Simple-Test.sql
@@ -0,0 +1,13 @@
+
+-- Name:   Simple Test
+-- Author: Tim Cross <tcross@une.edu.au>
+-- Date:   2019-02-10 14:41:33
+-- Source: rollback/Simple-Test.sql
+-- Type:   Rollback
+
+BEGIN;
+
+-- rollback of changes go here
+
+COMMIT;
+
---
+++ verify/Simple-Test.sql
@@ -0,0 +1,9 @@
+
+-- Name:   Simple Test
+-- Author: Tim Cross <tcross@une.edu.au>
+-- Date:   2019-02-10 14:41:33
+-- Source: verify/Simple-Test.sql
+-- Type:   Verify
+
+-- verify code go here
+
