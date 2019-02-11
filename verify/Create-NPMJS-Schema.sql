800b10304fa5292ef5ed9dc2e9e4131fb6ab5092
Tim Cross <theophilusx@gmail.com>
2019-02-11 16:34:31 +1100

    Reset to init state for testing

--- verify/Create-NPMJS-Schema.sql
+++
@@ -1,9 +0,0 @@
--- Name:   Create NPMJS Schema
--- Author: Tim Cross <tcross8@une.edu.au>
--- Date:   2019-02-11 15:57:05
--- Source: verify/Create-NPMJS-Schema.sql
--- Type:   Verify
-
-SELECT
-  HAS_SCHEMA_PRIVILEGE('dev_support', 'npmjs', 'usage');
-
