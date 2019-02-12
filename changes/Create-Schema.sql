5b538c50a25595d9bdc442117a0b4cf0ff8e677b
Tim Cross <tcross8@une.edu.au>
2019-02-12 12:44:34 +1100

    Submitting plan Create Schema for approval

---
+++ changes/Create-Schema.sql
@@ -0,0 +1,12 @@
+-- Name:   Create Schema
+-- Author: Tim Cross <tcross8@une.edu.au>
+-- Date:   2019-02-12 12:34:25
+-- Source: changes/Create-Schema.sql
+-- Type:   Changes
+
+BEGIN;
+
+CREATE SCHEMA IF NOT EXISTS npmjs AUTHORIZATION dev_support;
+
+COMMIT;
+
