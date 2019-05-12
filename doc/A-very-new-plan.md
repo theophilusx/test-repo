ab1cd5507e06ef5d28880c5d96670c832896aff0
Tim Cross <tcross8@une.edu.au>
2019-05-12 13:34:15 +1000

    Submitting plan A very new plan for approval

---
+++ doc/A-very-new-plan.md
@@ -0,0 +1,11 @@
+
+# A very new plan
+
+- **UUID** vqiL9jCgqhTxGuin7tVuB5 
+- **Created** 2019-05-12 10:25:56 
+- **Author** Tim Cross <tcross8@une.edu.au> 
+
+More testing
+
+-------
+
---
+++ rollback/A-very-new-plan.sql
@@ -0,0 +1,13 @@
+
+-- Name:   A very new plan
+-- Author: Tim Cross <tcross8@une.edu.au>
+-- Date:   2019-05-12 10:25:56
+-- Source: rollback/A-very-new-plan.sql
+-- Type:   Rollback
+
+BEGIN;
+
+-- rollback of changes go here
+
+COMMIT;
+
